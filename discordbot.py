d95f719268965c4816c8ab761840634deb12d348
tempestif <50777725+tempestif@users.noreply.github.com>
2020-01-29 15:02:25 +0900

    commit

--- discordbot.py
+++ discordbot.py
@@ -97,7 +97,7 @@ async def on_voice_state_update(member, before, after):
         now = datetime.utcnow() + timedelta(hours=9)
         alert_channel = client.get_channel(639850501780930580)
         if(after.channel != himitsu):
-            if .channel is None:
+            if before.channel is None:
             msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
             await alert_channel.send(msg)
         elif(before.channel != himitsu):
