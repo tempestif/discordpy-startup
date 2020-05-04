7aebc168bcf10a4b1f8e855d58bdf8cbbb57981d
tempestif <50777725+tempestif@users.noreply.github.com>
2020-05-04 16:19:08 +0900

    匿名

--- discordbot.py
+++ discordbot.py
@@ -206,7 +206,40 @@ async def D(ctx):
     for user in ctx.guild.members:
         if user.status == discord.Status.online:
             print (user.name+"#"+user.discriminator)
-    
+            
+#匿名メッセージ
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        # もし、送信者がbotなら無視する
+        return
+    GLOBAL_CH_NAME = "hoge-global" # グローバルチャットのチャンネル名
+
+    if message.channel.name == GLOBAL_CH_NAME:
+        # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
+
+        await message.delete() # 元のメッセージは削除しておく
+
+        channels = client.get_all_channels()
+        print(channels)
+        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
+        # channelsはbotの取得できるチャンネルのイテレーター
+        # global_channelsは hoge-global の名前を持つチャンネルのリスト
+
+        embed = discord.Embed(title="hoge-global",
+            description=message.content, color=0x00bfff)
+
+        embed.set_author(name=message.author.display_name,
+            icon_url=message.author.avatar_url_as(format="png"))
+        embed.set_footer(text=f"{message.guild.name} / {message.channel.name}",
+            icon_url=message.guild.icon_url_as(format="png"))
+        # Embedインスタンスを生成、投稿者、投稿場所などの設定
+
+        for channel in global_channels:
+            print(global_channels)
+            # メッセージを埋め込み形式で転送
+            await channel.send(embed=embed)
+            
 @client.command()
 async def biginning(bigin):
     bigining = discord.Embed(title='ようこそてんぺすとのお部屋へ！',description='以下の文章をお読みになってから先へ進んでくださいませ。',color=0xf0f8ff)
